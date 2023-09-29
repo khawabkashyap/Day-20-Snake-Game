fd11cc5f7c41e7d1f374b9941929524ea4a7f170
khwabkashyap <khawabkashyap@hotmail.com>
2023-09-29 20:48:00 +0530

    created a high score  tracker

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,9 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
+        f = open("data.txt", "r").read()
+        self.high_score = f
         self.penup()
         self.goto(x=0, y=270)
         self.color("white")
@@ -15,7 +18,7 @@ class ScoreBoard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALINGNMENT, font=FONT)
+        self.write(f"Score: {self.score}, High Score: {self.high_score}", align=ALINGNMENT, font=FONT)
 
     def game_over(self):
         self.goto(0, 0)
@@ -23,5 +26,9 @@ class ScoreBoard(Turtle):
 
     def increase_score(self):
         self.score += 1
+        if int(self.high_score) < self.score:
+            self.high_score = self.score
+            f = open("data.txt", "w")
+            f.write(str(self.high_score))
         self.clear()
         self.update_scoreboard()
